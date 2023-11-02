bf53bbd976da4a93b0d3f16b0b1ddf35b845a01e
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-11-01 21:58:16 -0500

    Corrected code

--- src/main/java/main/DiningExperienceManager.java
+++ src/main/java/main/DiningExperienceManager.java
@@ -1,118 +1,180 @@
 package main;
 
-import java.util.HashMap;
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@SuppressWarnings("PMD.UseUtilityClass")
-public class DiningExperienceManager {
+
+/**
+ * The DiningExperienceManager class 
+ *
+ * @author Enrique Zambrano
+ * @version 1.0
+ * @since 2023-11-01
+ */
+public final class DiningExperienceManager {
+	
+	/**
+	 * The base cost for dining orders.
+	 */
 	private static final double BASE_COST = 5.0;
+
+	/**
+	 * The discount applied for ordering more than 5 meals.
+	 */
 	private static final double DISCOUNT_5_MEALS = 0.10;
-	private static final double DISCOUNT_10_MEALS = 0.20;
-	private static final double DISCOUNT_OVER_50 = 10.0;
-	private static final double DISCOUNT_OVER_100 = 25.0;
+	/**
+	 * The discount applied for ordering more than 10 meals.
+	 */
+    private static final double DISCOUNT_10_MEALS = 0.20;
+	/**
+	 * The discount applied for ordering more than 50 meals.
+	 */
+    private static final double DISCOUNT_OVER_50 = 10.0;
+	/**
+	 * The discount applied for ordering more than 100 meals.
+	 */
+    private static final double DISCOUNT_OVER_100 = 25.0;
+	/**
+	 * The discount applies to orders equal to 50 meals.
+	 */
+    private static final double DISCOUNT_TD_50 = 50.0;
+	/**
+	 * The discount applies to orders equal to 100 meals.
+	 */
+    private static final double DISCOUNT_TD_100 = 100.0;
+	/**
+	 * The discount applies to orders equal to 10 meals.
+	 */
+    private static final double DISCOUNT_TD_10 = 10.0;
+	/**
+	 * The discount applies to orders equal to 5 meals.
+	 */
+    private static final double DISCOUNT_TD_5 = 5.0;
+	/**
+	 * Logger.
+	 */
+    private static final Logger logger = Logger.getLogger(DiningExperienceManager.class.getName());
+    private DiningExperienceManager() {
+        // Constructor privado para evitar la instanciación
+    }
+    /**
+     * This is the main method that serves as the entry point of the program.
+     *
+     * @param args The command-line arguments passed to the program.
+     */
+    public static void main(String[] args) {
+        final Map<String, Double> menu = createMenu();
+        final Map<String, Integer> order = new ConcurrentHashMap<>();
+        double totalCost = BASE_COST;
 
-	public static void main(String[] args) {
-		Map<String, Double> menu = createMenu();
-		Map<String, Integer> order = new HashMap<>();
-		double totalCost = BASE_COST;
+        try (Scanner scanner = new Scanner(System.in)) {
+            logger.info("Welcome to Dining Experience Manager!");
+            displayMenu(menu);
 
-		final Scanner scanner = new Scanner(System.in);
+            while (true) {
+                logger.info("Enter a meal from the menu (or 'done' to confirm): ");
+                final String meal = scanner.nextLine().trim().toLowerCase(Locale.ENGLISH);
+                final String done = "done";
+                if (done.equals(meal)) {
+                    break;
+                }
 
-		System.out.println("Welcome to Dining Experience Manager!");
-		displayMenu(menu);
+                if (!menu.containsKey(meal)) {
+                    logger.info("Invalid meal selection. Please select a meal from the menu.");
+                    continue;
+                }
+                if (logger.isLoggable(Level.INFO)) {
+                    logger.info("Enter the quantity for " + meal + ": ");
+                }
+                final int quantity = getValidQuantity(scanner);
 
-		while (true) {
-			System.out.print("Enter a meal from the menu (or 'done' to confirm): ");
-			String meal = scanner.nextLine().trim().toLowerCase();
+                if (quantity > 0 && quantity <= 100) {
+                    order.put(meal, quantity);
+                    totalCost += menu.get(meal) * quantity;
+                } else {
+                    logger.info("Invalid quantity. Please enter a positive integer between 1 and 100.");
+                }
+            }
+        }
 
-			if ("done".equals(meal)) {
-				break;
-			}
+        displayOrder(order);
+        final double discountedCost = applyDiscounts(order, totalCost);
 
-			if (!menu.containsKey(meal)) {
-				System.out.println("Invalid meal selection. Please select a meal from the menu.");
-				continue;
-			}
+        if (discountedCost >= 0) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info("Total cost: $" + (int) discountedCost);
+            }
+        } else {
+            logger.info("Order canceled or invalid input. Returning -1.");
+        }
+    }
 
-			System.out.print("Enter the quantity for " + meal + ": ");
-			int quantity = getValidQuantity(scanner);
+    private static Map<String, Double> createMenu() {
+        final Map<String, Double> menu = new ConcurrentHashMap<>();
+        menu.put("meal1", 10.0);
+        menu.put("meal2", 15.0);
+        menu.put("meal3", 20.0);
+        // Add more meals and prices as needed
+        return menu;
+    }
+    private static int getValidQuantity(final Scanner scanner) {
+        int quantity;
+        while (true) {
+            try {
+                quantity = Integer.parseInt(scanner.nextLine());
+                if (quantity > 0) {
+                    return quantity;
+                } else {
+                    logger.info("Please enter a positive integer greater than zero.");
+                }
+            } catch (NumberFormatException e) {
+                logger.info("Invalid input. Please enter a positive integer greater than zero.");
+            }
+        }
+    }
 
-			if (quantity > 0 && quantity <= 100) {
-				order.put(meal, quantity);
-				totalCost += menu.get(meal) * quantity;
-			} else {
-				System.out.println("Invalid quantity. Please enter a positive integer between 1 and 100.");
-			}
-		}
+    private static void displayOrder(final Map<String, Integer> order) {
+        logger.info("Selected Meals:");
+        for (final Map.Entry<String, Integer> entry : order.entrySet()) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info(entry.getKey() + " x" + entry.getValue());
+            }
+        }
+    }
+    private static void logMenu(final Map<String, Double> menu) {
+        for (final Map.Entry<String, Double> entry : menu.entrySet()) {
+            if (logger.isLoggable(Level.INFO)) {
+                logger.info(entry.getKey() + ": $" + entry.getValue());
+            }
+        }
+    }
 
-		displayOrder(order);
-		final double discountedCost = applyDiscounts(order, totalCost);
+    private static void displayMenu(final Map<String, Double> menu) {
+        logMenu(menu);
+    }
 
-		if (discountedCost >= 0) {
-			System.out.println("Total cost: $" + (int) discountedCost);
-		} else {
-			System.out.println("Order canceled or invalid input. Returning -1.");
-		}
-	}
+    private static double applyDiscounts(final Map<String, Integer> order, final double totalCost) {
+        final int totalQuantity = order.values().stream().mapToInt(Integer::intValue).sum();
+        double discountedCost = totalCost;
 
-	private static Map<String, Double> createMenu() {
-		Map<String, Double> menu = new HashMap<>();
-		menu.put("meal1", 10.0);
-		menu.put("meal2", 15.0);
-		menu.put("meal3", 20.0);
-		// Add more meals and prices as needed
-		return menu;
-	}
+        if (totalQuantity > DISCOUNT_TD_5) {
+            discountedCost *= 1 - DISCOUNT_5_MEALS;
+        }
 
-	private static void displayMenu(Map<String, Double> menu) {
-		System.out.println("Menu:");
-		for (Map.Entry<String, Double> entry : menu.entrySet()) {
-			System.out.println(entry.getKey() + ": $" + entry.getValue());
-		}
-	}
+        if (totalQuantity > DISCOUNT_TD_10) {
+            discountedCost *= 1 - DISCOUNT_10_MEALS;
+        }
 
-	private static int getValidQuantity(final Scanner scanner) {
-		int quantity;
-		while (true) {
-			try {
-				quantity = Integer.parseInt(scanner.nextLine());
-				if (quantity > 0) {
-					return quantity;
-				} else {
-					System.out.println("Please enter a positive integer greater than zero.");
-				}
-			} catch (NumberFormatException e) {
-				System.out.println("Invalid input. Please enter a positive integer greater than zero.");
-			}
-		}
-	}
+        if (discountedCost > DISCOUNT_TD_100) {
+            discountedCost -= DISCOUNT_OVER_100;
+        } else if (discountedCost > DISCOUNT_TD_50) {
+            discountedCost -= DISCOUNT_OVER_50;
+        }
 
-	private static void displayOrder(Map<String, Integer> order) {
-		System.out.println("Selected Meals:");
-		for (Map.Entry<String, Integer> entry : order.entrySet()) {
-			System.out.println(entry.getKey() + " x" + entry.getValue());
-		}
-	}
-
-	private static double applyDiscounts(Map<String, Integer> order, double totalCost) {
-		int totalQuantity = order.values().stream().mapToInt(Integer::intValue).sum();
-		double discountedCost = totalCost;
-
-		if (totalQuantity > 5) {
-			discountedCost *= 1 - DISCOUNT_5_MEALS;
-		}
-
-		if (totalQuantity > 10) {
-			discountedCost *= 1 - DISCOUNT_10_MEALS;
-		}
-
-		if (discountedCost > 100) {
-			discountedCost -= DISCOUNT_OVER_100;
-		} else if (discountedCost > 50) {
-			discountedCost -= DISCOUNT_OVER_50;
-		}
-
-		return discountedCost;
-	}
+        return discountedCost;
+    }
 }
